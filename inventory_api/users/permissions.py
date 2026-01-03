991585667da07dfad2f7684787644084e92c7b85
root <root@MAY.localdomain>
2026-01-03 22:38:18 +0100

    Updated the url file

---
+++ inventory_api/users/permissions.py
@@ -0,0 +1,22 @@
+from rest_framework.permissions import BasePermission, SAFE_METHODS
+
+
+class IsOwnerOrWorker(BasePermission):
+    """
+    Owners: full access
+    Workers: read + update quantity only
+    """
+
+    def has_permission(self, request, view):
+        return request.user.is_authenticated
+
+    def has_object_permission(self, request, view, obj):
+        if request.user.role == 'owner':
+            return True
+
+        if request.user.role == 'worker':
+            if request.method in SAFE_METHODS:
+                return True
+            return request.method in ['PUT', 'PATCH']
+
+        return False
--- inventory_api/users/views.py
+++ inventory_api/users/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+# users/views.py
+from rest_framework import viewsets
+from django.contrib.auth import get_user_model
+from rest_framework.permissions import IsAuthenticated 
+from .permissions import IsOwnerOrWorker
+from .serializers import UserSerializer
 
-# Create your views here.
+User = get_user_model()
+
+
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    permission_classes = [IsAuthenticated, IsOwnerOrWorker]
+
